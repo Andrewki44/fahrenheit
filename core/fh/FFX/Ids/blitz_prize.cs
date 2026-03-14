2fc365dfae4ac4916291c7bf559f968914e2f8b4
Andrewki44 <andrewkilbourn1996@gmail.com>
2026-03-15 04:21:53 +1100

    Create initial file & typedef

---
+++ core/fh/FFX/Ids/blitz_prize.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-License-Identifier: MIT
+
+/* [fkelava 13/9/22 08:12]
+ * source: MS Store ver.
+ * header: header_btl_command
+ *
+ * /ffx_ps2/ffx/proj2/chr/ath/battle/command.ath
+ *
+ * This was a real pain to translate.
+ */
+
+namespace Fahrenheit.FFX.Ids;
+
+public static class BlitzPrizeId {
+    public const T_XBlitzPrizeId BLTZ_ETHER                     = 0x0000;
+}
